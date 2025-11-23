93bb6bbf399c5ee1f52f1f41d9dd2e139c5f3ca1
fariganello <fariganello@gmail.com>
2025-11-22 22:49:16 -0300

    Feat: Add mobile touch support and responsive layout

--- src/core/input.js
+++ src/core/input.js
@@ -11,14 +11,36 @@ export class InputHandler {
 
     initListeners() {
         window.addEventListener("mousemove", (evt) => this.getMousePos(evt));
+
+        // Touch support
+        this.canvas.addEventListener("touchmove", (evt) => this.getTouchPos(evt), { passive: false });
+        this.canvas.addEventListener("touchstart", (evt) => this.getTouchPos(evt), { passive: false });
     }
 
     getMousePos(evt) {
         if (this.game.gameState !== 'PLAYING') return;
 
         let rect = this.canvas.getBoundingClientRect();
-        let newY = evt.clientY - rect.top - this.userPaddle.height / 2;
+        let scaleY = CANVAS_HEIGHT / rect.height;
+        let newY = (evt.clientY - rect.top) * scaleY - this.userPaddle.height / 2;
+        this.updatePaddlePos(newY);
+    }
 
+    getTouchPos(evt) {
+        if (this.game.gameState !== 'PLAYING') return;
+
+        // Prevent scrolling while playing
+        if (evt.cancelable) evt.preventDefault();
+
+        let rect = this.canvas.getBoundingClientRect();
+        let scaleY = CANVAS_HEIGHT / rect.height;
+        let touch = evt.touches[0];
+        let newY = (touch.clientY - rect.top) * scaleY - this.userPaddle.height / 2;
+
+        this.updatePaddlePos(newY);
+    }
+
+    updatePaddlePos(newY) {
         // Clamp User Position
         if (newY < 0) newY = 0;
         if (newY + this.userPaddle.height > CANVAS_HEIGHT) newY = CANVAS_HEIGHT - this.userPaddle.height;
